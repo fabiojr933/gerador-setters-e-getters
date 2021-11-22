797bfe00d00fdfd322671a6ed9912b44a73e8e1c
Fabio Pereira <fabiojr933@gmail.com>
2021-11-22 09:05:13 -0400

    geradores de getters e setters

--- js/script.js
+++ js/script.js
@@ -1,127 +1,134 @@
-/*
+var variablesArray = [];
 
-www.gunnarcorre.com
-This software was developed by Gunnar Correa, and is available on Github.
-*/
-var variablesArray =  [];
-
-function CreateData(){
-  if(GetVariables().length > 0){
-    GenerateClass();
-  }else{
-    UIkit.modal.alert('Invalid variable field!')
-  }
+function CreateData() {
+    if (GetVariables().length > 0) {
+        GenerateClass();
+    } else {
+        UIkit.modal.alert('Invalido!')
+    }
 }
 
-function GenerateClass(){
-  variablesArray = CreateVariables();
+function nomeClasse() {
+    if (GetVariables().length < 0) {
+        UIkit.modal.alert('Invalido');
+        return;
+    } else {
+        var doc = document.getElementById('nomeClasse').value;
+        doc = Ucfirst(doc);
+        return doc;
 
-  var data = [
-    "&lt;?php",
-    "<br>",
-    "\tnamespace ../../;",
-    "<br>",
-    "<br>",
-    "\tclass ClassName{",
-    "<br>",
-    "<br>",
-    DeclareVariables(),
-    "<br>",
-    Constructor(),
-    "<br>",
-    Setter(),
-    Getter(),
-    "\t}",
-    "<br>",
-    "?>",
-  ];
-
-  document.getElementById("finalResult").innerHTML = "";
-  for(var i = 0; i < data.length; i++){
-    document.getElementById("finalResult").innerHTML += data[i];
-  }
-  Syntax();
+    }
 }
 
-function Syntax(){
-  block = document.getElementsByTagName("code");
-  for(var i = 0; i < block.length; i++){
-    hljs.highlightBlock(block[i])
-  }
+function GenerateClass() {
+    variablesArray = CreateVariables();
+
+    var data = [
+        "&lt;?php",
+        "<br>",
+        "\tnamespace ../../;",
+        "<br>",
+        "<br>",
+        "\tclass Class", nomeClasse(), "{",
+        "<br>",
+        "<br>",
+        DeclareVariables(),
+        "<br>",
+        Constructor(),
+        "<br>",
+        Setter(),
+        Getter(),
+        "\t}",
+        "<br>",
+        "?>",
+    ];
+
+    document.getElementById("finalResult").innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+        document.getElementById("finalResult").innerHTML += data[i];
+    }
+    Syntax();
 }
 
-function GetVariables(){
-  return document.getElementById("txtVariables").value;
+function Syntax() {
+    block = document.getElementsByTagName("code");
+    for (var i = 0; i < block.length; i++) {
+        hljs.highlightBlock(block[i])
+    }
 }
 
-function CreateVariables(){
-  variablesArray = null;
-  var datas = (GetVariables().trim()).split("\n");
-  return datas;
+function GetVariables() {
+    return document.getElementById("txtVariables").value;
+}
+
+function CreateVariables() {
+    variablesArray = null;
+    var datas = (GetVariables().trim()).split("\n");
+    return datas;
 }
 
 function DeclareVariables() {
-  var variables = "";
-  for(var i = 0; i < variablesArray.length; i++){
-    variables += "\t\tprivate $" +variablesArray[i]+ ";<br>";
-  }
+    var variables = "";
+    for (var i = 0; i < variablesArray.length; i++) {
+        variables += "\t\tprivate $" + variablesArray[i] + ";<br>";
+    }
 
-  return variables;
+    return variables;
 }
 
-function Getter(){
-  if(!document.getElementById("cbGetter").checked)
-  return "";
-  var getter = "";
+function Getter() {
+    if (!document.getElementById("cbGetter").checked)
+        return "";
+    var getter = "";
 
-  for(var i = 0; i < variablesArray.length; i++){
-    var name = Ucfirst(variablesArray[i]);
-    getter += "\t\tpublic function get" + name + "(){<br>";
-    getter += "\t\t\t return $this->" + variablesArray[i] + ";<br>";
-    getter += "\t\t}<br><br>";
-  }
+    for (var i = 0; i < variablesArray.length; i++) {
+        var name = (variablesArray[i]);
+        getter += "\t\tpublic function get" + name + "(){<br>";
+        getter += "\t\t\t return $this->" + variablesArray[i] + ";<br>";
+        getter += "\t\t}<br><br>";
+    }
 
-  return getter;
+    return getter;
 }
 
-function Setter(){
-  if(!document.getElementById("cbSetter").checked)
-  return "";
+function Setter() {
+    if (!document.getElementById("cbSetter").checked)
+        return "";
 
-  var setter = "";
+    var setter = "";
 
-  for(var i = 0; i < variablesArray.length; i++){
-    var name = Ucfirst(variablesArray[i]);
-    setter += "\t\tpublic function set"+name+"($"+name+"){<br>";
-    setter += "\t\t\t$this->" + variablesArray[i] + " = $" +name + ";";
-    setter += "<br>\t\t}<br><br>";
-  }
+    for (var i = 0; i < variablesArray.length; i++) {
+        var name = (variablesArray[i]);
+        setter += "\t\tpublic function set" + name + "($" + name + "){<br>";
+        setter += "\t\t\t$this->" + variablesArray[i] + " = $" + name + ";";
+        setter += "<br>\t\t}<br><br>";
+    }
 
 
-  return setter;
+    return setter;
 }
 
-function Constructor(){
-  if(!document.getElementById("cbConstructor").checked)
-  return "";
+function Constructor() {
+    if (!document.getElementById("cbConstructor").checked)
+        return "";
 
-  var constructor = "";
+    var constructor = "";
 
-  constructor +="\t\tpublic function __construct("
+    constructor += "\t\tpublic function __construct("
 
-  for(var i = 0; i < variablesArray.length; i++){
-    constructor += "$" + variablesArray[i] + " = ''" + ((i+1) < variablesArray.length ? ", " : "") ;
-  }
-  constructor+= "){";
+    for (var i = 0; i < variablesArray.length; i++) {
+        constructor += "$" + variablesArray[i] + " = ''" + ((i + 1) < variablesArray.length ? ", " : "");
+    }
+    constructor += "){";
 
-  for(var i = 0; i < variablesArray.length; i++){
-    constructor += "<br>\t\t\t$this->" + variablesArray[i] + " = $" + variablesArray[i] + ";";
-  }
+    for (var i = 0; i < variablesArray.length; i++) {
+        constructor += "<br>\t\t\t$this->" + variablesArray[i] + " = $" + variablesArray[i] + ";";
+    }
 
-  constructor +="<br>\t\t}<br>";
-  return constructor;
+    constructor += "<br>\t\t}<br>";
+    return constructor;
 }
 
-function Ucfirst(string){
-  return string.charAt(0).toUpperCase() + string.slice(1);
+function Ucfirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
